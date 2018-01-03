28b94b4b6f153045e91ad019fba9da93f194fe8f
Anton Tkachov <lookformeon@gmail.com>
2018-01-03 05:22:27 +0200

    Fix rubocop warnings

--- app/controllers/followers_controller.rb
+++ app/controllers/followers_controller.rb
@@ -3,6 +3,6 @@
 # app/controllers/application_controller.rb
 class FollowersController < ApplicationController
   def index
-    @followers = [1,2,3,4,5]
+    @followers = [1, 2, 3, 4, 5]
   end
 end
